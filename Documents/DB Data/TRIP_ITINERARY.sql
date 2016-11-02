8480562cfd35457d910e39459057f0281d0c445b
Matt Lai <matthewlai.110@gmail.com>
2016-11-02 15:29:09 -0700

    More SQL files for the DB

---
+++ Documents/DB Data/TRIP_ITINERARY.sql
@@ -0,0 +1,31 @@
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (1, 1, '2016/10/30', 3, 0, 0, 1);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (1, 2, '2016/10/31', 1, 0, 0, 2);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (1, 3, '2016/11/01', 2, 0, 0, 3);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (1, 4, '2016/11/02', 1, 0, 0, 4);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (1, 5, '2016/11/03', 1, 0, 0, 5);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (1, 6, '2016/11/04', 1, 0, 0, 6);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (1, 7, '2016/11/05', 4, 0, 0, 7);
+
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (2, 1, '2016/12/05', 3, 0, 0, 8);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (2, 2, '2016/12/06', 1, 0, 0, 9);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (2, 3, '2016/12/07', 1, 0, 0, 10);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (2, 4, '2016/12/08', 1, 0, 0, 11);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (2, 5, '2016/12/09', 2, 0, 0, 12);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (2, 6, '2016/12/10', 1, 0, 0, 13);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (2, 7, '2016/12/11', 4, 0, 0, 14);
+
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (3, 1, '2016/11/15', 3, 0, 0, 15);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (3, 2, '2016/11/16', 1, 0, 0, 16);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (3, 3, '2016/11/17', 2, 0, 0, 17);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (3, 4, '2016/11/18', 1, 0, 0, 18);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (3, 5, '2016/11/19', 1, 0, 0, 19);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (3, 6, '2016/11/20', 1, 0, 0, 20);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (3, 7, '2016/11/21', 4, 0, 0, 21);
+
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (4, 1, '2016/12/25', 3, 0, 0, 22);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (4, 2, '2016/12/26', 1, 0, 0, 23);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (4, 3, '2016/12/27', 1, 0, 0, 24);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (4, 4, '2016/12/28', 2, 0, 0, 25);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (4, 5, '2016/12/29', 1, 0, 0, 26);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (4, 6, '2016/12/30', 1, 0, 0, 27);
+insert into TRIP_ITINERARY (trip_id, day_id, trip_date, status_id, isReroute, isCancelled, itin_id) values (4, 7, '2016/12/31', 4, 0, 0, 28);
---
+++ Documents/DB Data/TRIP_PORTS.sql
@@ -0,0 +1,15 @@
+insert into TRIP_PORTS (itin_id, port_id, depart_time) values (1, 5, '23:23:00');
+insert into TRIP_PORTS (itin_id, port_id, arrival_time, depart_time) values (3, 17, '12:09:00', '16:22:00');
+insert into TRIP_PORTS (itin_id, port_id, arrival_time) values (7, 13, '03:07:00');
+
+insert into TRIP_PORTS (itin_id, port_id, depart_time) values (8, 9, '23:23:00');
+insert into TRIP_PORTS (itin_id, port_id, arrival_time, depart_time) values (12, 12, '10:09:00', '15:22:00');
+insert into TRIP_PORTS (itin_id, port_id, arrival_time) values (14, 20, '20:12:00');
+
+insert into TRIP_PORTS (itin_id, port_id, depart_time) values (15, 16, '04:03:00');
+insert into TRIP_PORTS (itin_id, port_id, arrival_time, depart_time) values (17, 8, '12:09:00', '16:22:00');
+insert into TRIP_PORTS (itin_id, port_id, arrival_time) values (21, 9, '20:12:00');
+
+insert into TRIP_PORTS (itin_id, port_id, depart_time) values (22, 3, '23:23:00');
+insert into TRIP_PORTS (itin_id, port_id, arrival_time, depart_time) values (25, 19, '08:09:00', '15:22:00');
+insert into TRIP_PORTS (itin_id, port_id, arrival_time) values (28, 1, '03:07:00');
