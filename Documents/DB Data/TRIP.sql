02d1819fea189eaf72da6b811bfc8ef852322621
Matthew Lai <matthewlai.110@gmail.com>
2016-11-03 14:24:31 -0700

    Updated SQL files for ROOM_PASSENGER and PASSENGER. New SQL files for new tables.

---
+++ Documents/DB Data/TRIP.sql
@@ -0,0 +1,4 @@
+insert into TRIP (trip_id, ship_id, departure_port, departure_date, destination_port) values (1, 1, 7, '2016/10/30', 17);
+insert into TRIP (trip_id, ship_id, departure_port, departure_date, destination_port) values (2, 2, 7, '2016/12/05', 18);
+insert into TRIP (trip_id, ship_id, departure_port, departure_date, destination_port) values (3, 1, 14, '2016/11/15', 6);
+insert into TRIP (trip_id, ship_id, departure_port, departure_date, destination_port) values (4, 1, 7, '2016/12/25', 8);
