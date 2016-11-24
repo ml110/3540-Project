b8dfd5e420fb887ca0e05cc5ada4ad72ce017d4e
Matt Lai <matthewlai.110@gmail.com>
2016-11-23 19:18:21 -0800

    Implemented the transaction log in DrinkForm. Removed old code from GiftForm. Removed unnecessary classes from GiftForm. Rearragned button methods in GiftForm.

--- Ship Program/Gifts_Control/Gifts_Control/GiftForm.Designer.cs
+++ Ship Program/Gifts_Control/Gifts_Control/GiftForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Drinks_control
+﻿namespace Gifts_Control
 {
-    partial class DrinkForm
+    partial class GiftForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,97 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.label5 = new System.Windows.Forms.Label();
-			this.dgvDisplay = new System.Windows.Forms.DataGridView();
-			this.Add = new System.Windows.Forms.Button();
+			this.AddBtn = new System.Windows.Forms.Button();
+			this.MinBtn = new System.Windows.Forms.Button();
+			this.label6 = new System.Windows.Forms.Label();
 			this.num = new System.Windows.Forms.TextBox();
-			this.minus = new System.Windows.Forms.Button();
+			this.dgvDisplay = new System.Windows.Forms.DataGridView();
 			this.btnLog = new System.Windows.Forms.Button();
 			this.btnInvControl = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.dgvDisplay)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// label5
+			// AddBtn
 			// 
-			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(-3, 475);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(84, 13);
-			this.label5.TabIndex = 5;
-			this.label5.Text = "Inventry Control ";
+			this.AddBtn.Location = new System.Drawing.Point(105, 470);
+			this.AddBtn.Name = "AddBtn";
+			this.AddBtn.Size = new System.Drawing.Size(75, 23);
+			this.AddBtn.TabIndex = 6;
+			this.AddBtn.Text = "Increase";
+			this.AddBtn.UseVisualStyleBackColor = true;
+			this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+			// 
+			// MinBtn
+			// 
+			this.MinBtn.Location = new System.Drawing.Point(105, 499);
+			this.MinBtn.Name = "MinBtn";
+			this.MinBtn.Size = new System.Drawing.Size(75, 23);
+			this.MinBtn.TabIndex = 10;
+			this.MinBtn.Text = "Decrease";
+			this.MinBtn.UseVisualStyleBackColor = true;
+			this.MinBtn.Click += new System.EventHandler(this.MinBtn_Click);
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(-1, 474);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(87, 13);
+			this.label6.TabIndex = 16;
+			this.label6.Text = "Inventory Control";
+			// 
+			// num
+			// 
+			this.num.Location = new System.Drawing.Point(-1, 490);
+			this.num.Name = "num";
+			this.num.Size = new System.Drawing.Size(100, 20);
+			this.num.TabIndex = 22;
 			// 
 			// dgvDisplay
 			// 
 			this.dgvDisplay.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
 			this.dgvDisplay.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
 			this.dgvDisplay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dgvDisplay.Location = new System.Drawing.Point(0, 3);
+			this.dgvDisplay.Location = new System.Drawing.Point(1, 3);
 			this.dgvDisplay.Name = "dgvDisplay";
 			this.dgvDisplay.Size = new System.Drawing.Size(713, 460);
-			this.dgvDisplay.TabIndex = 12;
-			// 
-			// Add
-			// 
-			this.Add.Location = new System.Drawing.Point(87, 469);
-			this.Add.Name = "Add";
-			this.Add.Size = new System.Drawing.Size(75, 23);
-			this.Add.TabIndex = 13;
-			this.Add.Text = "Increase";
-			this.Add.UseVisualStyleBackColor = true;
-			this.Add.Click += new System.EventHandler(this.Add_Click);
-			// 
-			// num
-			// 
-			this.num.Location = new System.Drawing.Point(0, 491);
-			this.num.Name = "num";
-			this.num.Size = new System.Drawing.Size(81, 20);
-			this.num.TabIndex = 14;
-			// 
-			// minus
-			// 
-			this.minus.Location = new System.Drawing.Point(87, 498);
-			this.minus.Name = "minus";
-			this.minus.Size = new System.Drawing.Size(75, 23);
-			this.minus.TabIndex = 15;
-			this.minus.Text = "Decrease";
-			this.minus.UseVisualStyleBackColor = true;
-			this.minus.Click += new System.EventHandler(this.minus_Click);
+			this.dgvDisplay.TabIndex = 23;
 			// 
 			// btnLog
 			// 
-			this.btnLog.Location = new System.Drawing.Point(626, 469);
+			this.btnLog.Location = new System.Drawing.Point(619, 470);
 			this.btnLog.Name = "btnLog";
-			this.btnLog.Size = new System.Drawing.Size(87, 52);
-			this.btnLog.TabIndex = 19;
+			this.btnLog.Size = new System.Drawing.Size(94, 51);
+			this.btnLog.TabIndex = 24;
 			this.btnLog.Text = "Show Transaction History";
 			this.btnLog.UseVisualStyleBackColor = true;
 			this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
 			// 
 			// btnInvControl
 			// 
-			this.btnInvControl.Location = new System.Drawing.Point(533, 469);
+			this.btnInvControl.Location = new System.Drawing.Point(519, 469);
 			this.btnInvControl.Name = "btnInvControl";
-			this.btnInvControl.Size = new System.Drawing.Size(87, 52);
-			this.btnInvControl.TabIndex = 20;
+			this.btnInvControl.Size = new System.Drawing.Size(94, 51);
+			this.btnInvControl.TabIndex = 25;
 			this.btnInvControl.Text = "Show Ship Inventory";
 			this.btnInvControl.UseVisualStyleBackColor = true;
-			this.btnInvControl.Click += new System.EventHandler(this.btnInvControl_Click);
 			// 
-			// DrinkForm
+			// GiftForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(715, 523);
 			this.Controls.Add(this.btnInvControl);
 			this.Controls.Add(this.btnLog);
-			this.Controls.Add(this.minus);
-			this.Controls.Add(this.num);
-			this.Controls.Add(this.Add);
 			this.Controls.Add(this.dgvDisplay);
-			this.Controls.Add(this.label5);
-			this.Name = "DrinkForm";
-			this.Text = "CRUISE CONTROL: Bar Inventory Control";
-			this.Load += new System.EventHandler(this.DrinkForm_Load);
+			this.Controls.Add(this.num);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.MinBtn);
+			this.Controls.Add(this.AddBtn);
+			this.Name = "GiftForm";
+			this.Text = "CRUISE CONTROL: Gift Inventory Control";
+			this.Load += new System.EventHandler(this.GiftForm_Load);
 			((System.ComponentModel.ISupportInitialize)(this.dgvDisplay)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
@@ -127,11 +126,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.DataGridView dgvDisplay;
-        private System.Windows.Forms.Button Add;
+		private System.Windows.Forms.Button AddBtn;
+		private System.Windows.Forms.Button MinBtn;
+		private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox num;
-		private System.Windows.Forms.Button minus;
+		private System.Windows.Forms.DataGridView dgvDisplay;
 		private System.Windows.Forms.Button btnLog;
 		private System.Windows.Forms.Button btnInvControl;
     }
--- Ship Program/Gifts_Control/Gifts_Control/GiftForm.cs
+++ Ship Program/Gifts_Control/Gifts_Control/GiftForm.cs
@@ -10,24 +10,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
-namespace Drinks_control
+namespace Gifts_Control
 {
-    public partial class DrinkForm : Form
+    public partial class GiftForm : Form
     {
+
         private MySqlConnection connection;
         MySqlCommand cmd;
 
-        int tID; //TRIP
-        int sID; //SHIP
+		int tID; //TRIP
+		int sID; //SHIP
 
-        public DrinkForm()
+        public GiftForm()
         {
             InitializeComponent();
-            cmd = new MySqlCommand();
+            cmd = new MySqlCommand();    
         }
 
-        //REALESE CONSTRUCTOR
-        public DrinkForm(MySqlConnection CONN, MySqlCommand CMD, int TN, int SN)
+		//REALESE CONSTRUCTOR
+        public GiftForm(MySqlConnection CONN, MySqlCommand CMD, int TN, int SN)
         {
             InitializeComponent();
             connection = CONN;
@@ -52,12 +53,6 @@ namespace Drinks_control
 			tID = 1;
 			sID = 1;
         }
-       
-        private void DrinkForm_Load(object sender, EventArgs e)
-        {
-			DBConnect(); //remove from release version
-			displayInv();
-        }
 
         private void setupSqlCommand(String query)
         {
@@ -65,12 +60,17 @@ namespace Drinks_control
             cmd.CommandText = query;
         }
 
-		//moving some code here to create a seperate method for inventory display
+		private void GiftForm_Load(object sender, EventArgs e)
+		{
+			DBConnect();
+			displayInv();
+		}
+
 		private void displayInv()
 		{
-			string query = "select DRINKS.drink_id AS ID, drink_name AS DRINK, drink_price AS PRICE, inventory AS STOCK from DRINKS, DRINK_INV ";
-			query += " where DRINKS.drink_id = DRINK_INV.drink_id and DRINK_INV.ship_id = '" + sID + "'";
-			
+			string query = "select GIFTS.gift_id AS ID, gift_name AS GIFT, gift_price AS PRICE, inventory AS STOCK from GIFTS, GIFT_INV ";
+			query += " where GIFTS.gift_id = GIFT_INV.gift_id and GIFT_INV.ship_id = '" + sID + "'";
+
 			DataSet DS = new DataSet();
 			dgvDisplay.DataSource = null;
 			MySqlDataAdapter MDA;
@@ -85,17 +85,17 @@ namespace Drinks_control
 			int s = dgvDisplay.SelectedCells[0].RowIndex;
 			DataGridViewRow selectedRow = dgvDisplay.Rows[s];
 			string a = Convert.ToString(selectedRow.Cells[3].Value);
-			//MessageBox.Show("Inventry before update: "+a);
+			//MessageBox.Show(a);
 
 			int number = int.Parse(num.Text);
 
 			int result = number + int.Parse(a);
 			dgvDisplay.Rows[s].Cells[3].Value = result.ToString();
-			//MessageBox.Show("Inventry after update: "+result.ToString());
+			//MessageBox.Show(result.ToString());
 
 			string a1 = Convert.ToString(selectedRow.Cells[0].Value);
 
-			string query = " UPDATE DRINK_INV SET inventory = " + result + " WHERE drink_id = " + a1 + " and ship_id = '" + sID + "'";
+			string query = " UPDATE GIFT_INV SET inventory = " + result + " WHERE GIFT_id = " + a1 + " and ship_id = 1 ";
 			setupSqlCommand(query);
 			cmd.ExecuteNonQuery();
 
@@ -104,7 +104,7 @@ namespace Drinks_control
 			num.Text = "";
 		}
 
-        private void Add_Click(object sender, EventArgs e)
+        private void AddBtn_Click(object sender, EventArgs e)
         {
 			try
 			{
@@ -118,7 +118,6 @@ namespace Drinks_control
 			{
 				MessageBox.Show("There was an error updating the inventory. Please contact an Administrator", "OOPS", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
-			
         }
 
 		private void decInv()
@@ -126,25 +125,26 @@ namespace Drinks_control
 			int s = dgvDisplay.SelectedCells[0].RowIndex;
 			DataGridViewRow selectedRow = dgvDisplay.Rows[s];
 			string a = Convert.ToString(selectedRow.Cells[3].Value);
-			//MessageBox.Show("Inventory before update:" +a);
+			//MessageBox.Show("Inventry after update: " + a);
 
 			int number = int.Parse(num.Text);
 
 			int result = int.Parse(a) - number;
 			dgvDisplay.Rows[s].Cells[3].Value = result.ToString();
-			//MessageBox.Show("Inventory after update: "+result.ToString());
+			//MessageBox.Show("Inventry after update: " + result.ToString());
 
 			string a1 = Convert.ToString(selectedRow.Cells[0].Value);
 
-			string query = " UPDATE DRINK_INV SET inventory = " + result + " WHERE drink_id = " + a1 + " and ship_id = '" + sID + "'";
+			string query = " UPDATE GIFT_INV SET inventory = " + result + " WHERE gift_id = " + a1 + " and ship_id = 1 ";
 			setupSqlCommand(query);
 			cmd.ExecuteNonQuery();
 
+			// MessageBox.Show(query);
 			displayInv();
 			num.Text = "";
 		}
 
-        private void minus_Click(object sender, EventArgs e)
+        private void MinBtn_Click(object sender, EventArgs e)
         {
 			try
 			{
@@ -162,13 +162,13 @@ namespace Drinks_control
 
 		private void showLog()
 		{
-			string logQuery = "SELECT concat(BT.sale_date, \" \", BT.sale_time) AS SALETIME, D.drink_name AS DRINK, D.drink_price AS PRICE,  concat(P.pass_firstname, \" \", P.pass_lastname) AS CUSTOMER, WA.area_name AS BAR, concat(S.staff_firstname, \" \", S.staff_lastname) AS BARTENDER FROM BAR_TRANSACTION AS BT";
-				logQuery += " INNER JOIN DRINKS AS D ON BT.drink_id = D.drink_id";
-				logQuery += " INNER JOIN ROOM_PASSENGER AS RP ON BT.roomPass_id = RP.roomPass_id";
+			string logQuery = "SELECT concat(GT.sale_date, \" \", GT.sale_time) AS SALETIME, G.gift_name AS GIFT, G.gift_price AS PRICE,  concat(P.pass_firstname, \" \", P.pass_lastname) AS CUSTOMER, WA.area_name AS SHOP, concat(S.staff_firstname, \" \", S.staff_lastname) AS STAFF FROM GIFT_TRANSACTION AS GT";
+				logQuery += " INNER JOIN GIFTS AS G ON GT.gift_id = G.gift_id";
+				logQuery += " INNER JOIN ROOM_PASSENGER AS RP ON GT.roomPass_id = RP.roomPass_id";
 				logQuery += " INNER JOIN ROOM AS R ON RP.room_id = R.room_id";
 				logQuery += " INNER JOIN PASSENGER AS P ON RP.pass_id = P.pass_id";
-				logQuery += " INNER JOIN STAFF AS S ON BT.bartender_id = S.staff_id";
-				logQuery += " INNER JOIN WORKAREAS AS WA ON BT.bar_id = WA.area_id";
+				logQuery += " INNER JOIN STAFF AS S ON GT.staff_id = S.staff_id";
+				logQuery += " INNER JOIN WORKAREAS AS WA ON GT.shop_id = WA.area_id";
 				logQuery += " WHERE RP.trip_id = '" + tID + "'";
 				logQuery += " ORDER BY SALETIME";
 
@@ -184,18 +184,18 @@ namespace Drinks_control
 		private void btnLog_Click(object sender, EventArgs e)
 		{
 			showLog();
-			minus.Enabled = false;
-			Add.Enabled = false;
+			MinBtn.Enabled = false;
+			AddBtn.Enabled = false;
 			num.Enabled = false;
 		}
 
 		private void btnInvControl_Click(object sender, EventArgs e)
 		{
 			displayInv();
-			minus.Enabled = true;
-			Add.Enabled = true;
+			MinBtn.Enabled = true;
+			AddBtn.Enabled = true;
 			num.Enabled = true;
 		}
-
+        
     }
 }
